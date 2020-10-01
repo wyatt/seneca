9b334311e732ac32032e40f9dd9a01ff3d816350
Wyatt Sell <profwyatt@gmail.com>
2020-10-01 15:56:42 +0000

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='senecalearning',
-    version='1.0.3',
+    version='1.0.4',
     author="Wyatt Sell",
     author_email="wyatt@wyattsell.com",
     packages=["senecalearning"],
