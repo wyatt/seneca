0b6f2c58e332b2940f85ad338a2255a838fa7141
Version bump and upload <profwyatt@gmail.com>
2020-10-12 09:24:09 +0000

    Bump version: 1.0.17 → 1.0.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='senecalearning',
-    version='1.0.17',
+    version='1.0.18',
     author="Wyatt Sell",
     author_email="wyatt@wyattsell.com",
     packages=["senecalearning"],
